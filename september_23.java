259c88abefd9b517e380ba139f3967842675b91e
RoHiT-engi <79554502+RoHiT-engi@users.noreply.github.com>
2023-09-25 19:49:46 +0530

    25/9/23

--- september_23.java
+++ september_23.java
@@ -782,5 +782,22 @@ class September_23 {
         // System.out.println(Arrays.deepToString(dp));
         return Math.min(dp[ro][query_glass],1.0);
     }
-
+    ///********************************************************************************************************* */
+    //!25/9/23 
+    public char findTheDifference(String s, String t) {
+        int count[]=new int[26];
+        for(int i=0;i<t.length();i++)
+        {
+            count[t.charAt(i)-'a']++;
+        }
+        for(int i=0;i<s.length();i++)
+        {
+            count[s.charAt(i)-'a']--;
+        }
+        
+        for(int i=0;i<26;i++){
+            if(count[i]>0) return (char)('a'+i);
+        }
+        return 'a';
+    }
 }
